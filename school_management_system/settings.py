235fbae80e42e7f97a0f3bf8281c5cc9fb081458
ITtechnical <68113347+ITtechnical@users.noreply.github.com>
2021-05-09 02:18:03 +0100

    c

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -171,6 +171,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 # MEDIA_LOCATION = 'media'
 # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
-# MEDIA_ROOT = '/%s/' % MEDIA_LOCATION
-DEFAULT_FILE_STORAGE = 'school_management_system.storage_backends.MediaStorage'
 
+DEFAULT_FILE_STORAGE = 'school_management_system.storage_backends.MediaStorage'
+MEDIA_ROOT = '/%s/' % DEFAULT_FILE_STORAGE
